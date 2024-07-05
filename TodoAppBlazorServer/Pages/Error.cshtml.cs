635d6fd8247d94870305a3db10222eebcacdab28
SteffWin <steffen.winternheimer@gmail.com>
2024-07-05 14:59:02 +0200

    Add Test API-Calls

--- TodoAppBlazorServer/Pages/Error.cshtml.cs
+++ TodoAppBlazorServer/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace TodoAppBlazorServer.Pages
+namespace LMS.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
---
+++ TodoAppBlazorServer/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS
+{
+    [Route("/")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("getdata")]
+        public IActionResult GetData()
+        {
+            return Ok(new { data = new { foo = "blablub" } });
+        }
+
+        [HttpGet("getdata2")]
+        public IActionResult GetData2()
+        {
+            return Ok(new { data = new { foo = "blablub2" } });
+        }
+
+        [HttpPost("postdata")]
+        public IActionResult PostData([FromBody] string data)
+        {
+            var response = new { message = "Hello from POST", receivedData = data };
+            return Ok(response);
+        }
+    }
+}
